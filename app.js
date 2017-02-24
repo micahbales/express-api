5669bb8dd50bd44ed27cc4b947f5643b8103ba96
Micah Bales <micahbales@gmail.com>
2017-02-24 09:07:12 -0500

    add GET route with parameters

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 
-var app = express();
+const app = express();
 
-app.get('/', function (req, res) {
-  res.send("Well, that worked just fine!");
-}).listen(3000);
+app.get('/', function(req, res) {
+  res.send("Root Directory");
+});
+
+app.get('/users/:id', function(req, res) {
+  let id = req.params.id;
+  let color = req.query.color;
+
+  res.send(`Yes? You asked for customer ${id} and passed the color ${color}.`)
+});
+
+app.listen(3000);
 
 console.log("Express listening on port 3000");
