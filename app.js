b6c0a53671af7af3e3661c61a9a0c3e0296794e6
Micah Bales <micahbales@gmail.com>
2017-02-25 15:43:01 -0500

    add routes/api

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 mongoose.connect('mongodb://localhost/rest_test');
+
+const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', function(req, res) {
-  res.send("Working");
-});
+app.use('/api', require('./routes/api'));
 
 app.listen(3000);
 
