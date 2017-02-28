b57165c108062937150ecbe8c22351224a9fe180
Micah Bales <micahbales@gmail.com>
2017-02-28 14:47:19 -0500

    replace deprecated code and reset default name

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res) {
-  let name = req.param('name', 'friend');
+  let defaultName = 'friend';
+  let name = req.params.name || defaultName;
   let hi = `Hello, and welcome, ${name}!`
   res.send(hi);
-  console.log(req.param('hi'));
+  console.log(req.params.hi || defaultName);
 });
 
 module.exports = router;
